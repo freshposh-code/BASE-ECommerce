1e0f9ff16359a7a738247429c9a551031568dff5
freshposh-code <atomisefarouk919@gmail.com>
2024-05-04 09:51:35 +0100

    commit changes

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,20 @@
+'use client'
+
+import Login from '@/components/Admin-panel/Login'
+import { useAppSelector } from '@/components/Redux/hooks'
+import { useSession } from 'next-auth/react'
 import React from 'react'
 
 const layout = () => {
+
+
+    const isLoading = useAppSelector(store => store.LoadingReducer)
+    const { data: session } = useSession()
+
+    if (!session?.user) {
+        return <Login />
+    }
+
     return (
         <div>layout</div>
     )
