5009ae76039cc58bc8ed57bf1eefae703bbb5e6b
freshposh-code <atomisefarouk919@gmail.com>
2024-05-04 09:50:35 +0100

    initial commit

---
+++ src/components/Admin-panel/Login.tsx
@@ -0,0 +1,14 @@
+import { signIn } from 'next-auth/react';
+import React from 'react'
+import { FcGoogle } from "react-icons/fc";
+
+const Login = () => {
+    return (
+        <div className='bg-black min-h-screen grid place-items-center'>
+            <button className='bg-white text-black px-8 py-4 flex gap-2 items-center' onClick={() => signIn("google")}>
+                <FcGoogle size={30} /> Sign in with Google</button>
+        </div>
+    )
+}
+
+export default Login 
