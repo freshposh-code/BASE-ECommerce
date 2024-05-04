e04ffd8559335ed47b0a5035dbe2ff37c6857415
freshposh-code <atomisefarouk919@gmail.com>
2024-05-04 15:03:19 +0100

    commit changes

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,13 +1,12 @@
 'use client'
 
 import Login from '@/components/Admin-panel/Login'
+import Sidebar from '@/components/Admin-panel/Sidebar'
 import { useAppSelector } from '@/components/Redux/hooks'
 import { useSession } from 'next-auth/react'
-import React from 'react'
+import Loader from './Loader'
 
-const layout = () => {
-
-
+const layout = ({ children }: { children: React.ReactNode }) => {
     const isLoading = useAppSelector(store => store.LoadingReducer)
     const { data: session } = useSession()
 
@@ -16,7 +15,17 @@ const layout = () => {
     }
 
     return (
-        <div>layout</div>
+        <div className='flex'>
+            <Sidebar />
+            <div className="w-full h-full">
+                {/* <Navbar /> */}
+
+                <div className="bg-gray-200 p-4 h-[calc(100vh-64px)]">
+                    {children}
+                </div>
+            </div>
+            {isLoading && <Loader />}
+        </div>
     )
 }
 
---
+++ src/components/Admin-panel/Sidebar.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { GrTransaction } from 'react-icons/gr'
+import { IoAnalytics } from 'react-icons/io5'
+import { MdAccountTree, MdDashboard } from 'react-icons/md'
+
+const menus = [
+    {
+        title: "Dashboard",
+        icon: <MdDashboard />,
+        href: "/admin/dashboard",
+    },
+    {
+        title: "Product",
+        icon: <AiOutlineShoppingCart />,
+        href: "/admin/dashboard",
+    },
+    {
+        title: "Accounts",
+        icon: <MdAccountTree />,
+        href: "/admin/dashboard",
+    },
+    {
+        title: "Transaction",
+        icon: <GrTransaction />,
+        href: "/admin/dashboard",
+    },
+    {
+        title: "Analytics",
+        icon: <IoAnalytics />,
+        href: "/admin/dashboard",
+    },
+    {
+        title: "setting",
+        icon: <MdDashboard />,
+        href: "/admin/dashboard",
+    },
+]
+const Sidebar = () => {
+    const pathName = usePathname();
+
+    return <div className='bg-white w-[300px] min-h-screen p-4 shrink-0'>
+        <div className="flex items-center gap-4">
+            <img className='size-12 rounded-lg' src="/logo.jpg" alt="logo" />
+            <h2 className='text-[20px] font-semibold'>Posh Association</h2>
+        </div>
+
+        <ul className="space-y-4 mt-6">
+            {menus.map((menu) => <Link key={menu.title} href={menu.href}
+                className={`flex gap-2 items-center p-4 rounded-lg cursor-pointer hover:bg-pink hover:text-white
+                 ${pathName !== menu.href ? "bg-pink text-white" : "bg-gray-200"}`}>
+
+                <div className="text-[20px]">{menu.icon}</div>
+                <p>{menu.title}</p>
+            </Link>)}
+        </ul>
+    </div>
+}
+
+export default Sidebar
