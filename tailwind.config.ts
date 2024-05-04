12aeae28899c0b5859ffd195b1750679d50050d9
freshposh-code <atomisefarouk919@gmail.com>
2024-05-04 15:02:53 +0100

    extend configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        accent: "#0989FF",
+        topHeadingPrimary: "#010f1c",
+        topHeadingSecondary: "#021d35",
+        pink: "#FD4B6B",
+      },
+
+      container: {
+        center: true,
+        padding: "15px"
+      }
     },
   },
   plugins: [],
