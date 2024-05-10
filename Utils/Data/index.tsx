bf983a354da828fac79f24a8712f01a56b6b261e
freshposh-code <atomisefarouk919@gmail.com>
2024-05-10 17:50:05 +0100

    add data to array

--- Utils/Data/index.tsx
+++ Utils/Data/index.tsx
@@ -1,7 +1,9 @@
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { GrTransaction } from "react-icons/gr";
 import { IoAnalytics } from "react-icons/io5";
-import { MdAccountTree, MdDashboard } from "react-icons/md";
+import { MdAccountTree, MdDashboard, MdSupportAgent } from "react-icons/md";
+import { RiRefund2Fill } from "react-icons/ri";
+import { TbDiscount, TbTruckDelivery } from 'react-icons/tb'
 
 export const menus = [
     {
@@ -33,5 +35,28 @@ export const menus = [
         title: "setting",
         icon: <MdDashboard />,
         href: "/setting",
+    },
+]
+
+export const features = [
+    {
+        icon: <TbTruckDelivery className="text-4xl" />,
+        title: "Free Delivery",
+        desc: "Orders from all item",
+    },
+    {
+        icon: <RiRefund2Fill className="text-4xl" />,
+        title: "Return & Refund",
+        desc: "Money back guarantee",
+    },
+    {
+        icon: <TbDiscount className="text-4xl" />,
+        title: "Member Discount",
+        desc: "On Order Over $99.00",
+    },
+    {
+        icon: <MdSupportAgent className="text-4xl" />,
+        title: "Support 24/7",
+        desc: "Contact us 24 hours a day",
     },
 ]
